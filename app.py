58ed83224e10f396d614008d787344bec5e5e8ba
Viet Nguyen <ntviet91@yahoo.com>
2019-02-19 13:00:35 +0200

    add explanation to app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config["UPLOAD_FOLDER"] = "uploads"
 def index():
     return redirect("/static/index.html")
 
+# With POST / a file is stored in the filesystem and the file's metadata (name, size etc) is stored in a DB.
 @app.route("/sendfile", methods=["POST"])
 def send_file():
     upfile = request.files["uploadedfile"]
@@ -23,19 +24,21 @@ def send_file():
         con.commit()
     return "successful_upload"
 
+# With GET / the list of uploaded files is retrieved from the database.
 @app.route("/filenames", methods=["GET"])
 def get_filenames():
     filenames = os.listdir("uploads/")
     return_dict = dict(filenames=filenames)
     return jsonify(return_dict)
 
+# With GET /a_file_from_the_list the file's contents are read using 'cat' command line utility and then sent as a response to the client.
 @app.route("/<file_from_list>", methods=["GET"])
 def read_filefromlist(file_from_list):
     if file_from_list not in os.listdir("uploads/"):
         return "No such file in system"
     else:
         file_path = "uploads/{}".format(file_from_list)
-        file_read = subprocess.check_output("cat {}".format(file_path), shell = True)
+        file_read = subprocess.check_output("cat {}".format(file_path), shell = True) # --> CAT command
         return file_read
         
 if __name__ == "__main__":
